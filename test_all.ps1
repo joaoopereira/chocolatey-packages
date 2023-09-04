b79695181197e4ce7015e5ee03b11da2ae005843
João Pereira <joao.octavio.pereira@gmail.com>
2023-09-04 01:00:46 +0100

    build: set schedule trigger reset all changes after test_all

--- test_all.ps1
+++ test_all.ps1
@@ -53,7 +53,7 @@ $options = [ordered]@{
         Params= @{                                          #Report parameters:
             Github_UserRepo = $Env:github_user_repo         #  Markdown: shows user info in upper right corner
             NoAppVeyor  = $true                             #  Markdown: do not show AppVeyor build shield
-            Title       = "Update Force Test - Group ${n}"
+            Title       = "Update Force Test"
             UserMessage = "[Ignored](#ignored) | [Update report](https://gist.github.com/$Env:gist_id) | **USING AU NEXT VERSION**"       #  Markdown, Text: Custom user message to show
         }
     }
