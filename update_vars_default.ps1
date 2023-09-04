3c05c6da5d1fdee94509d06491c084a48cd9c149
João Pereira <joao.octavio.pereira@gmail.com>
2023-09-04 02:02:15 +0100

    chore: add variables to update_vars_default

--- update_vars_default.ps1
+++ update_vars_default.ps1
@@ -1,15 +1,21 @@
 # AU Packages Template: https://github.com/majkinetor/au-packages-template
 # Copy this file to update_vars.ps1 and set the variables there. Do not include it in the repository.
 
-$Env:mail_user        = ''
-$Env:mail_pass        = ''
-$Env:mail_server      = 'smtp.gmail.com'
-$Env:mail_port        = '587'
-$Env:mail_enablessl   = 'true'
+$Env:mail_user              = ''
+$Env:mail_pass              = ''
+$Env:mail_server            = 'smtp.gmail.com'
+$Env:mail_port              = '587'
+$Env:mail_enablessl         = 'true'
 
-$Env:api_key          = ''          #Chocolatey api key
-$Env:gist_id          = ''          #Specify your gist id or leave empty for anonymous gist
-$Env:gist_id_test     = ''          #Specify your gist id for test runs or leave empty for anonymous gist
-$Env:github_user_repo = ''          #{github_user}/{repo}
-$Env:github_api_key   = ''          #Github personal access token
-$Env:au_Push          = 'false'     #Push to chocolatey
+$Env:api_key                = ''          #Chocolatey api key
+$Env:gist_id                = ''          #Specify your gist id or leave empty for anonymous gist
+$Env:gist_id_test           = ''          #Specify your gist id for test runs or leave empty for anonymous gist
+$Env:github_user_repo       = ''          #{github_user}/{repo}
+$Env:github_api_key         = ''          #Github personal access token
+$Env:au_Push                = 'false'     #Push to chocolatey
+$Env:au_Force               = 'false'
+$Env:au_NoCheckChocoVersion = 'false'
+
+$Env:default_branch         = 'main'
+$Env:force_git              = 'false'
+$Env:force_gitReleases      = 'false'
